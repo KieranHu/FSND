c36a32c678ea3cb1a6082a9b1e1bcff9776f4789
Kaiyuan Hu <kaiyuanhu94@gmail.com>
2017-07-06 19:47:04 -0400

    add Project 4 item category

---
+++ P4_Item_Category/items_db.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Items(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(1000), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    item = relationship(Category)
+
+engine = create_engine('sqlite:///item.db')
+Base.metadata.create_all(engine)
