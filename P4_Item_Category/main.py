6ef82a5672448666cb556e3c67ab5709cb533b30
Kaiyuan Hu <kaiyuanhu@gmail.com>
2017-07-12 22:35:07 -0400

    update add google auth

--- P4_Item_Category/main.py
+++ P4_Item_Category/main.py
@@ -1,4 +1,5 @@
 import os
+import string
 
 from flask import Flask, render_template, url_for, request, redirect, flash, jsonify, abort
 from flask import session as login_session
@@ -8,7 +9,19 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import desc
 
 from items_db import Base, Category, Items
-from users_db import User
+# from users_db import User
+
+import random
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in '%s': %s" % (cwd, files))
 
 # load database
 engine = create_engine('sqlite:///item.db')
@@ -17,53 +30,168 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-engine_user = create_engine('sqlite:///user.db')
-Base.metadata.bind = engine_user
-
-DBSession_user = sessionmaker(bind=engine_user)
-session_user = DBSession_user()
+# engine_user = create_engine('sqlite:///user.db')
+# Base.metadata.bind = engine_user
+#
+# DBSession_user = sessionmaker(bind=engine_user)
+# session_user = DBSession_user()
 
 app = Flask(__name__)
 
 # root for login
-@app.route('/login/', methods = ['GET', "POST"])
+# @app.route('/login/', methods = ['GET', "POST"])
+# def login():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         pwd = request.form['password']
+#         if username is None or pwd is None:
+#             abort(400)
+#         login_session['logged_in'] = False
+#         user = session_user.query(User).filter_by(name=username).first()
+#         if user.verify_password(pwd):
+#             login_session['logged_in'] = True
+#
+#         return redirect(url_for('show_main'))
+#     else:
+#         return render_template('login.html')
+#
+# @app.route('/logout/')
+# def logout():
+#     login_session['logged_in'] = False
+#     return redirect(url_for('show_main'))
+
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+
+# Create anti-forgery state token
+@app.route('/login')
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        pwd = request.form['password']
-        if username is None or pwd is None:
-            abort(400)
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in range(32))
+    login_session['state'] = state
+    return render_template('new_login.html', STATE=state)
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
         login_session['logged_in'] = False
-        user = session_user.query(User).filter_by(name=username).first()
-        if user.verify_password(pwd):
-            login_session['logged_in'] = True
+        return response
+    # Obtain authorization code
+    code = request.data
 
-        return redirect(url_for('show_main'))
-    else:
-        return render_template('login.html')
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        login_session['logged_in'] = False
+        return response
 
-@app.route('/logout/')
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        login_session['logged_in'] = False
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        login_session['logged_in'] = False
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print
+        "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        login_session['logged_in'] = False
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        login_session['logged_in'] = True
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+    login_session['logged_in'] = True
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print("done!")
+    return output
+
+    # DISCONNECT - Revoke a current user's token and reset their login_session
+
+
+@app.route('/gdisconnect')
 def logout():
+    del login_session['access_token']
+    del login_session['gplus_id']
+    del login_session['username']
+    del login_session['picture']
     login_session['logged_in'] = False
+
     return redirect(url_for('show_main'))
 
-@app.route('/create_account/', methods=['GET', "POST"])
-def create_account():
-    if request.method == 'POST':
-        username = request.form['username']
-        pwd = request.form['password']
-        if username is None or pwd is None:
-            abort(400)
-        if session_user.query(User).filter_by(name=username).first() is not None:
-            abort(400)  # existing user
-        user = User(name=username)
-        user.hash_passward(pwd)
-        session_user.add(user)
-        session_user.commit()
-        return redirect(url_for('login'))
-
-    else:
-        return render_template('create_account.html')
+# @app.route('/create_account/', methods=['GET', "POST"])
+# def create_account():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         pwd = request.form['password']
+#         if username is None or pwd is None:
+#             abort(400)
+#         if session_user.query(User).filter_by(name=username).first() is not None:
+#             abort(400)  # existing user
+#         user = User(name=username)
+#         user.hash_passward(pwd)
+#         session_user.add(user)
+#         session_user.commit()
+#         return redirect(url_for('login'))
+#
+#     else:
+#         return render_template('create_account.html')
 
 # API
 
@@ -232,4 +360,4 @@ def delete_item(item_name):
 if __name__ == '__main__':
     app.debug = True
     app.secret_key = os.urandom(12)
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='localhost', port=5000)
